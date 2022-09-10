342ae6c8cbc3b3409418068ddac85dba0807aa5c
codeswitchingstl <codeswitching@icloud.com>
2022-09-10 16:22:27 -0500

    initial commit

---
+++ FunWithQuizzes/Questions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp
+{
+    public abstract class Question
+    {
+        public string QuestionText { get; set; }
+        public List<int> CorrectAnswer { get; set; }
+        public List<string> PossibleAnswers { get; set; }
+        public static int PointValue;
+        public Question (string question, List<int> answer, List<string> possibleAnswers)
+        {
+            QuestionText = question;
+            CorrectAnswer = answer;
+            PossibleAnswers = possibleAnswers;
+        }
+
+        // method to display questions
+        public abstract void DisplayQuestion();
+
+        // method to display possible answer choices
+       // public abstract void DisplayChoices();
+
+        // method to display answers
+        public abstract void GetAnswer();
+    }
+}
